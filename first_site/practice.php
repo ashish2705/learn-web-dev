c2db6a2dd3e62faab567768a5496e17d9e3d1b3d
ashish2705 <ashishjaiswal.iitd@gmail.com>
2020-06-04 18:11:50 +0530

    cahnges

---
+++ first_site/practice.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+	<title>first code</title>
+</head>
+<body id='bd' style="background-color:red">
+<?php 
+	document.body.style.background=$_GET["clr"]; 
+?>
+</body>
+</html>
